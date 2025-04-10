6fb4eb98657f6f9a67a999e9e46f7fce960ef6b4
Tadeo <diego.tadeo4922@alumnos.udg.mx>
2025-04-09 22:30:20 -0600

    Home screen and animated background

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,24 +1,75 @@
-import { Link } from "expo-router";
-import { View, Text, SafeAreaView } from "react-native";
+import React from 'react';
+import { Image, StyleSheet, TouchableOpacity, Text, View, SafeAreaView } from 'react-native';
+import AnimatedBackground from '@/assets/components/AnimatedBackground';
+import { useRouter } from 'expo-router'; 
 
 export default function HomeScreen() {
+  const router = useRouter();
   return (
-    <SafeAreaView className="bg-white h-full">
-      <View className="flex flex-col px-5 my-20 items-center justify-center">
-        <Text className="font-bold font-biryani-black text-4xl py-5 mt-10">
-          PartyApp
-        </Text>
-        <Text className="font-bold text-xl mt-10">Home Screen</Text>
-        <Link className="mt-10" href={"/screens/user-profile"}>
-          <Text className="font-bold text-xl">Mi Perfil</Text>
-        </Link>
-        <Link className="mt-10" href={"/screens/sign-in"}>
-          <Text className="font-bold text-xl">Sign In</Text>
-        </Link>
-        <Link className="mt-10" href={"/screens/fonts"}>
-          <Text className="font-bold text-2xl">Fuentes</Text>
-        </Link>
-      </View>
-    </SafeAreaView>
+      <SafeAreaView style={styles.safeArea}>
+      <AnimatedBackground />
+        <View style={styles.container}>
+          <View style={styles.topContent}>
+          <Text style={[styles.title, { fontFamily: 'KronaOne-Regular' }]}>PartyApp</Text>
+            <Image source={require('@/assets/images/logo-principal.png')} style={styles.logo} />
+          </View>
+
+          <View style={styles.bottomButtons}>
+            <TouchableOpacity style={styles.button} onPress={() => router.push("/screens/main-window")}>
+              <Text style={styles.buttonText}>Iniciar sesión</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity style={styles.button}>
+              <Text style={styles.buttonText}>Registrarme</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  gradientBackground: {
+    flex: 1,
+  },
+  safeArea: {
+    flex: 1,
+  },
+  container: {
+    flex: 1,
+    justifyContent: 'space-between',
+    paddingHorizontal: 24,
+  },
+  topContent: {
+    alignItems: 'center',
+    marginTop: 80,
+  },
+  title: {
+    fontSize: 32,
+    fontWeight: 'bold',
+    color: 'white',
+    marginBottom: 10,
+  },
+  logo: {
+    width: 80,
+    height: 80,
+    marginBottom: 30,
+  },
+  bottomButtons: {
+    alignItems: 'center',
+    marginBottom: 50,
+  },
+  button: {
+    backgroundColor: 'rgba(255, 255, 255, 0.2)',
+    paddingVertical: 12,
+    paddingHorizontal: 40,
+    borderRadius: 25,
+    marginTop: 15,
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 17,
+    fontFamily: 'Biryani-Regular',
+    fontWeight: 'bold',
+  },
+});
---
+++ app/screens/main-window.tsx
@@ -0,0 +1,24 @@
+import { Link } from "expo-router";
+import { View, Text, SafeAreaView } from "react-native";
+
+export default function HomeScreen() {
+  return (
+    <SafeAreaView className="bg-white h-full">
+      <View className="flex flex-col px-5 my-20 items-center justify-center">
+        <Text className="font-bold font-biryani-black text-4xl py-5 mt-10">
+          PartyApp
+        </Text>
+        <Text className="font-bold text-xl mt-10">Home Screen</Text>
+        <Link className="mt-10" href={"/screens/user-profile"}>
+          <Text className="font-bold text-xl">Mi Perfil</Text>
+        </Link>
+        <Link className="mt-10" href={"/screens/sign-in"}>
+          <Text className="font-bold text-xl">Sign In</Text>
+        </Link>
+        <Link className="mt-10" href={"/screens/fonts"}>
+          <Text className="font-bold text-2xl">Fuentes</Text>
+        </Link>
+      </View>
+    </SafeAreaView>
+  );
+}
