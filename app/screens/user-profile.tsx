fd4f4462f3a41acd62b81bfed7649c101ed87f68
Tadeo <diego.tadeo4922@alumnos.udg.mx>
2025-04-16 23:12:25 -0600

    Added navbar as a component, improved user profile view, home screen, and login screen

--- app/screens/user-profile.tsx
+++ app/screens/user-profile.tsx
@@ -8,11 +8,20 @@ import {
   TouchableOpacity,
 } from "react-native";
 import { StatusBar } from "expo-status-bar";
+import { LinearGradient } from 'expo-linear-gradient';
+import { FontAwesome } from "@expo/vector-icons";
+import BottomNavBar from '../../assets/components/navBar';
 
 const ProfileScreen: React.FC = () => {
   return (
-    <SafeAreaView className="bg-gray-100 h-full py-8">
-      <StatusBar style="dark" />
+    <LinearGradient
+    colors={['#4A60C9', '#4A60C9', '#4A60C9', '#9663BA', '#9663BA']}
+    style={{ flex: 1 }}
+    start={{ x: 0, y: 0 }}
+    end={{ x: 0, y: 1 }}
+    >
+    <SafeAreaView>
+      <StatusBar style="light" />
       <ScrollView
         contentContainerStyle={{
           paddingBottom: 40,
@@ -28,81 +37,91 @@ const ProfileScreen: React.FC = () => {
               width: 120,
               height: 120,
               borderRadius: 60,
-              marginBottom: 10,
+              marginBottom: 5,
             }}
           />
 
+            {/* Estrellas de calificación */}
+            <View className="flex-row mt-1 mb-2">
+              <FontAwesome name="star" size={20} color="#FFD700" />
+              <FontAwesome name="star" size={20} color="#FFD700" />
+              <FontAwesome name="star" size={20} color="#FFD700" />
+              <FontAwesome name="star-half-full" size={20} color="#FFD700" />
+              <FontAwesome name="star-o" size={20} color="#FFD700" />
+            </View>
+
+
           {/* Título de perfil */}
-          <Text className="text-3xl font-bold text-gray-800">Mi Perfil</Text>
+          <Text className="text-3xl font-bold text-white" style={{marginTop: 5}}>Nombre Del Usuario</Text>
 
           {/* Botón editar perfil */}
           <TouchableOpacity
-            className="bg-blue-500 px-5 py-2 rounded-full mt-2"
-            style={{ marginBottom: 15 }}
+            className="bg-violet-500 px-5 py-2 rounded-md mt-2"
+            style={{ marginBottom: 15, marginTop: 15}}
           >
             <Text className="text-white font-semibold">Editar Perfil</Text>
           </TouchableOpacity>
 
           {/* Información básica */}
-          <View className="bg-white p-6 rounded-xl shadow-md w-11/12 max-w-md">
-            <Text className="text-lg font-semibold text-gray-700">Nombre:</Text>
-            <Text className="text-xl text-gray-900">X</Text>
+          <View className="bg-slate-800 p-6 rounded-xl shadow-md w-11/12 max-w-md">
+            <Text className="text-lg font-semibold text-white">Nombre:</Text>
+            <Text className="text-xl text-white">X</Text>
           </View>
 
-          <View className="bg-white p-6 rounded-xl shadow-md w-11/12 max-w-md mt-4">
-            <Text className="text-lg font-semibold text-gray-700">
+          <View className="bg-slate-800 p-6 rounded-xl shadow-md w-11/12 max-w-md mt-4">
+            <Text className="text-lg font-semibold text-white">
               Correo Electrónico:
             </Text>
-            <Text className="text-xl text-gray-900">correo@example.com</Text>
+            <Text className="text-xl text-white">correo@example.com</Text>
           </View>
 
-          <View className="bg-white p-6 rounded-xl shadow-md w-11/12 max-w-md mt-4">
-            <Text className="text-lg font-semibold text-gray-700">
+          <View className="bg-slate-800 p-6 rounded-xl shadow-md w-11/12 max-w-md mt-4">
+            <Text className="text-lg font-semibold text-white">
               Teléfono:
             </Text>
-            <Text className="text-xl text-gray-900">+52 123 456 7890</Text>
+            <Text className="text-xl text-white">+52 123 456 7890</Text>
           </View>
 
           {/* Registro */}
-          <View className="bg-white p-6 rounded-xl shadow-md w-11/12 max-w-md mt-4">
-            <Text className="text-lg font-semibold text-gray-700">
+          <View className="bg-slate-800 p-6 rounded-xl shadow-md w-11/12 max-w-md mt-4">
+            <Text className="text-lg font-semibold text-white">
               INE Digital:
             </Text>
             <Text className="text-xl text-green-600">Verificado</Text>
           </View>
 
-          <View className="bg-white p-6 rounded-xl shadow-md w-11/12 max-w-md mt-4">
-            <Text className="text-lg font-semibold text-gray-700">
+          <View className="bg-slate-800 p-6 rounded-xl shadow-md w-11/12 max-w-md mt-4">
+            <Text className="text-lg font-semibold text-white">
               Método de Pago:
             </Text>
-            <Text className="text-xl text-gray-900">
+            <Text className="text-xl text-white">
               Tarjeta de débito ***1234
             </Text>
           </View>
 
           {/* Eventos próximos */}
-          <View className="bg-white p-6 rounded-xl shadow-md w-11/12 max-w-md mt-4">
-            <Text className="text-lg font-semibold text-gray-700 mb-3">
+          <View className="bg-slate-800 p-6 rounded-xl shadow-md w-11/12 max-w-md mt-4">
+            <Text className="text-lg font-semibold text-white mb-3">
               Eventos Próximos:
             </Text>
             <View className="mb-2">
-              <Text className="text-lg font-medium text-gray-800">
+              <Text className="text-lg font-medium text-white">
                 Fiesta 1 - 20 de abril de 2025
               </Text>
-              <Text className="text-sm text-gray-500">Grupo contratado: X</Text>
+              <Text className="text-sm text-white">Grupo contratado: X</Text>
             </View>
             <View className="mb-2">
-              <Text className="text-lg font-medium text-gray-800">
+              <Text className="text-lg font-medium text-white">
                 Fiesta Corporativa - 30 de mayo de 2025
               </Text>
-              <Text className="text-sm text-gray-500">Grupo contratado: X</Text>
+              <Text className="text-sm text-white">Grupo contratado: X</Text>
             </View>
           </View>
 
           {/* Historial de eventos */}
-          <View className="bg-white p-6 rounded-xl shadow-md w-11/12 max-w-md mt-4">
+          <View className="bg-slate-800 p-6 rounded-xl shadow-md w-11/12 max-w-md mt-4">
             <View className="flex-row justify-between items-center mb-3">
-              <Text className="text-lg font-semibold text-gray-700">
+              <Text className="text-lg font-semibold text-white">
                 Historial de Eventos:
               </Text>
               <TouchableOpacity>
@@ -113,34 +132,34 @@ const ProfileScreen: React.FC = () => {
             </View>
 
             <View className="mb-2">
-              <Text className="text-lg font-medium text-gray-800">
+              <Text className="text-lg font-medium text-white">
                 Cumpleaños - 10 de enero de 2025
               </Text>
-              <Text className="text-sm text-gray-500">
+              <Text className="text-sm text-white">
                 Grupo contratado: Mariachi Real
               </Text>
             </View>
 
             <View className="mb-2">
-              <Text className="text-lg font-medium text-gray-800">
+              <Text className="text-lg font-medium text-white">
                 Graduación - 15 de diciembre de 2024
               </Text>
-              <Text className="text-sm text-gray-500">
+              <Text className="text-sm text-white">
                 Grupo contratado: Banda Norteña
               </Text>
             </View>
           </View>
 
           {/* Calificaciones */}
-          <View className="bg-white p-6 rounded-xl shadow-md w-11/12 max-w-md mt-4 mb-6">
-            <Text className="text-lg font-semibold text-gray-700 mb-3">
+          <View className="bg-slate-800 p-6 rounded-xl shadow-md w-11/12 max-w-md mt-4 mb-6">
+            <Text className="text-lg font-semibold text-white mb-3">
               Mis Calificaciones:
             </Text>
             <View className="mb-2">
-              <Text className="text-lg font-medium text-gray-800">
+              <Text className="text-lg font-medium text-white">
                 Grupo: X ⭐⭐⭐⭐⭐
               </Text>
-              <Text className="text-sm text-gray-500">
+              <Text className="text-sm text-white">
                 ¡Excelente anfitrión!
               </Text>
             </View>
@@ -154,7 +173,9 @@ const ProfileScreen: React.FC = () => {
           </TouchableOpacity>
         </View>
       </ScrollView>
+      <BottomNavBar activeTab="perfil" />
     </SafeAreaView>
+    </LinearGradient>
   );
 };
 
